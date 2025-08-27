33e6cd7a8451748ca76c93c36f8dc86a159b097e
PauloR777 <modexshot123@gmail.com>
2025-08-27 21:42:23 +0700

    Add script part

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -54,7 +54,7 @@ export default {
             color: 'red-5',
             textColor: 'white',
             icon: 'warning',
-            message: 'คุณจำเป็นต้องยอมรับ'
+            message: 'You need to accept the license and terms first'
           })
         }
         else {
@@ -62,7 +62,7 @@ export default {
             color: 'green-4',
             textColor: 'white',
             icon: 'cloud_done',
-            message: 'ข้อมูลได้รับการยืนยัน'
+            message: 'Submitted'
           })
         }
       },
